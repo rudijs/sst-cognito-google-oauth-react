4ca2baef788e8baccaedb21d15462ec3db201686
Rudi Starcevic <rudi.starcevic@pm.me>
2022-08-13 15:17:35 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sst-cognito-google-oauth-react
+
 Serverless Stack Auth Cognito OAuth React Demo
+
+##
+
+https://sst.dev/examples/how-to-add-google-login-to-your-cognito-user-pool.html
---
+++ frontend/src/main.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Amplify } from "@aws-amplify/core"
+import "./index.css"
+import App from "./App"
+
+/*
+console.log(import.meta.env.VITE_APP_REGION)
+console.log(import.meta.env.VITE_APP_USER_POOL_ID)
+console.log(import.meta.env.VITE_APP_USER_POOL_CLIENT_ID)
+console.log(`${import.meta.env.VITE_APP_COGNITO_DOMAIN}.auth.${import.meta.env.VITE_APP_REGION}.amazoncognito.com`)
+console.log(2, import.meta.env.VITE_APP_API_STAGE)
+const redirectSignIn = import.meta.env.VITE_APP_API_STAGE === "prod" ? "production-url" : "http://localhost:3000"
+console.log("redirectSignIn :>> ", redirectSignIn)
+console.log(import.meta.env.VITE_APP_API_URL)
+console.log(import.meta.env.VITE_APP_REGION)
+*/
+
+// Configure AWS Amplify with credentials from backend
+Amplify.configure({
+  Auth: {
+    region: import.meta.env.VITE_APP_REGION,
+    userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
+    userPoolWebClientId: import.meta.env.VITE_APP_USER_POOL_CLIENT_ID,
+    mandatorySignIn: false,
+    oauth: {
+      domain: `${import.meta.env.VITE_APP_COGNITO_DOMAIN + ".auth." + import.meta.env.VITE_APP_REGION + ".amazoncognito.com"}`,
+      scope: ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
+      redirectSignIn: import.meta.env.VITE_APP_API_STAGE === "prod" ? "production-url" : "http://localhost:5173", // Make sure to use the exact URL
+      redirectSignOut: import.meta.env.VITE_APP_API_STAGE === "prod" ? "production-url" : "http://localhost:5173", // Make sure to use the exact URL
+      responseType: "token",
+    },
+  },
+  API: {
+    endpoints: [
+      {
+        name: "api",
+        endpoint: import.meta.env.VITE_APP_API_URL,
+        region: import.meta.env.VITE_APP_REGION,
+      },
+    ],
+  },
+})
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
